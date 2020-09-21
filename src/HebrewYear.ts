d511acf2444eeb9efddb001b18f1bd1cb80b252a
Tzahi Levi <thetzahilevi@gmail.com>
2020-09-21 16:08:22 +0300

    make things more object-oriented

--- src/HebrewYear.ts
+++ src/HebrewYear.ts
@@ -1,159 +1,202 @@
-import * as Common from './Common'
+import {
+    dayOfRoshHashana,
+    DAYS_WEEK,
+    daysInMonth,
+    daysInYear,
+    fixedDate,
+    gimatria,
+    YEAR_MONTHS,
+    YEARBETWEENGEROGIANTOJEWISH,
+    YearType
+} from './Common'
 import parshatShavoa from './ParashatShavoa'
 
 
 export interface IHebDay {
-    year: number;
+    year: HebrewYear;
     day: Date;
     hebDay: number;
     hebMonth: number;
     parasha: string;
-    leapYear: boolean;
     weekDay: number;
-    isFullMonth: () => boolean;
     toString: () => string;
 }
-const MONTHES = ['', 'תשרי', 'חשון', 'כסלו', 'טבת', 'שבט', 'אדר', "אדר ב'", 'ניסן', 'אייר', 'סיון', 'תמוז', 'אב', 'אלול'];
-const WEEKDAY = ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת',];
+const MONTHES: Array<'' | YEAR_MONTHS> = ['', YEAR_MONTHS.TESHRI, YEAR_MONTHS.HESVAN, YEAR_MONTHS.KESLEV,
+    YEAR_MONTHS.TEVET, YEAR_MONTHS.SHVAT, YEAR_MONTHS.ADAR, YEAR_MONTHS.ADARB,
+    YEAR_MONTHS.NISAN, YEAR_MONTHS.IAR, YEAR_MONTHS.SIVAN, YEAR_MONTHS.TAMUZ,
+    YEAR_MONTHS.AV, YEAR_MONTHS.ELOL];
+const WEEKDAY = [DAYS_WEEK.RESON, DAYS_WEEK.SHENI, DAYS_WEEK.SHLISHI,
+    DAYS_WEEK.REVEI, DAYS_WEEK.HAMESHI, DAYS_WEEK.SHISHI, DAYS_WEEK.SHABAT];
 
 function findRoshHashana(d): Date {
-    let r = Common.dayOfRoshHashana(d.getFullYear());
-    if (r.getTime() - d > 0) r = Common.dayOfRoshHashana(d.getFullYear() - 1);
+    let r = dayOfRoshHashana(d.getFullYear());
+    if (r.getTime() - d > 0) r = dayOfRoshHashana(d.getFullYear() - 1);
     return r;
 }
 
 export default class HebrewYear {
     rosh: Date;
-    year: number;
-    days: number;
+    hebYear: number;
+    daysInYear: number;
     leap: boolean;
-    type: string;
-    montes: HebrewDay[];
+    type: YearType;
+    weeksWithParash: number;
+    days: Map<string, HebrewDay>;
 
 
     constructor(date) {
-        this.rosh = findRoshHashana(Common.fixedDate(new Date(date)));
-        this.year = this.rosh.getFullYear() + Common.YEARBETWEENGEROGIANTOJEWISH;
-        this.days = Common.daysInYear(this.year);
-        this.leap = this.days > 380;
-        this.type = Common.getYearType(this.year);
-        this.montes = createYear(this.rosh, this.leap, this.days, this.type, this.year);
+        this.createYear(date)
+    }
 
-
-        function createYear(_rosh, leap, days, type, year): IHebDay[] {
-            let roshHashhanaDay = _rosh.getDay();
-            let day = new Date(_rosh);
-            let a: IHebDay[] = [];
-            let w = 0;
-            MONTHES.forEach( (month, m) => {
-                if(m == 0 || (month == MONTHES[7] && !leap)) return;
-                let dayInMonth = Common.daysInMonth(month, leap, days);
-                for( let i = 1; i<= dayInMonth; i++){
-                    let p = getParasha(i, month);
-                    a.push(new HebrewDay(day, i, m, year, p, leap, dayInMonth ));
-                    day.setDate(day.getDate() + 1)
-                }
-            });
-            return a;
-
-            function getParasha(d: number, m: string) {
-                if(m == MONTHES[1]) {
-                    if(d < 23) return getParashaBeforeBershit(d);
-                    else if(roshHashhanaDay === 6 && d < 29) {
-                        return 'בראשית'
-                    }
-                }
-                if (m == MONTHES[8]) {
-                    switch (type[2]) {
-                        case 'א': {
-                            if (d >= 15 && d <= 21) return 'פסח';
-                            break;
-                        }
-                        case 'ג': {
-                            if (d >= 13 && d <= 19) return 'פסח';
-                            break;
-                        }
-                        case 'ה': {
-                            if (d >= 11 && d <= 17) return 'פסח';
-                            break;
-                        }
-                        case 'ז': {
-                            if (d >= 9 && d <= 15) return 'פסח';
-                            break;
-                        }
-                        default: {
-                            if (day.getDay() == 0) { w++; }
-                            return parshatShavoa(type, w)
-                        }
-                    }
-                }
-                if(type[0] === 'ז' && (m == MONTHES[13] && d >= 24)) return 'ראש השנה'
-                if (day.getDay() == 0) { w++; }
-                return parshatShavoa(type, w)
-
-            }
-            /**
-             * get parasha from rosh hashana to simhat tora
-             */
-            function getParashaBeforeBershit(d): string {
-                switch (roshHashhanaDay) {
-                    case 1: {
-                        if (d >= 1 && d <= 6) return 'וילך';
-                        else if (d >= 7 && d <= 13) return 'האזינו';
-                        else if (d >= 14 && d <= 20) return 'סוכות';
-                        else return 'בראשית';
-                    }
-                    case 2: {
-                        if (d >= 1 && d <= 5) return 'וילך';
-                        else if (d >= 6 && d <= 12) return 'האזינו';
-                        else if (d >= 13 && d <= 19) return 'סוכות';
-                        else return 'בראשית';
-                    }
-                    case 4: {
-                        if (d >= 1 && d <= 3) return 'האזינו';
-                        else if (d >= 4 && d <= 10) return 'יום הכיפורים';
-                        else if (d >= 11 && d <= 17) return 'סוכות';
-                        else return 'בראשית';
-                    }
-
-                    case 6: {
-                        if (d == 1) return 'ראש השנה';
-                        else if (d >= 2 && d <= 8) return 'האזינו';
-                        else if (d >= 9 && d <= 22) return 'סוכות';
-                        else return 'בראשית';
-                    }
-                    default: return '';
-                }
+    private createYear(date) {
+        this.rosh = findRoshHashana(fixedDate(new Date(date)));
+        this.hebYear = this.rosh.getFullYear() + YEARBETWEENGEROGIANTOJEWISH;
+        this.daysInYear = daysInYear(this.hebYear);
+        this.leap = this.daysInYear > 380;
+        this.type = new YearType(this.rosh, this.daysInYear, this.leap);
+        this.days = new Map();
+        this.weeksWithParash = 0;
+        let day = new Date(this.rosh);
+        for(let m = 1; m <= MONTHES.length - 1; m++) {
+            const month: YEAR_MONTHS = MONTHES[m] as YEAR_MONTHS;
+            if(month === YEAR_MONTHS.ADARB && !this.leap) continue;
+            let _daysInMonth = daysInMonth(month, this.leap, this.daysInYear);
+            for( let i = 1; i <= _daysInMonth; i++) {
+                const hebDay = new HebrewDay(day, i, m, this);
+                this.days.set(day.toDateString(), hebDay);
+                day.setDate(day.getDate() + 1);
             }
         }
     }
-    
+
+    getDay(date: Date | string): HebrewDay | undefined {
+        if(typeof date === 'string') {
+            date = new Date(date);
+        }
+        if(!this.days.has(date.toDateString())) {
+            this.createYear(date);
+        }
+        return this.days.get(date.toDateString());
+    }
+
+    getPesahDay() {
+        return this.type.getPesahDayInWeek();
+    }
+
+    getRoshHashanaDay() {
+        return this.type.getRoshHashanaDayInWeek();
+    }
+
+    getYearLength() {
+        return this.type.getYearLength();
+    }
+
+    checkIfMonthIsFull(month: number) {
+        if( month < 1 || month > 13) {
+            throw new Error('no such month');
+        }
+        let _daysInMonth = daysInMonth(MONTHES[month] as YEAR_MONTHS, this.leap, this.daysInYear);
+        return _daysInMonth === 30;
+    }
 }
 
-
 class HebrewDay implements IHebDay{
-    year: number;
     day: Date;
+    year: HebrewYear;
     hebDay: number;
     hebMonth: number;
     parasha: string;
-    leapYear: boolean;
     weekDay: number;
-    isFullMonth: () => boolean;
-    constructor(d, d1, m, y, p, l, dim) {
+
+    constructor(date, day, month, y) {
         this.year = y;
-        this.day = new Date(d)
-        this.hebDay = d1;
-        this.hebMonth = m;
-        this.parasha = p;
-        this.leapYear = l;
+        this.day = new Date(date);
+        this.hebDay = day;
+        this.hebMonth = month;
         this.weekDay = this.day.getDay() ;
-        this.isFullMonth = () => {
-            return dim == 30;
+        this.parasha = this.getParasha();
+    }
+
+    /**
+     * @deprecated use checkIfMonthIsFull from the year instance
+     */
+    isFullMonth() {
+        return this.year.checkIfMonthIsFull(this.hebMonth);
+    }
+    toString(): string {
+        return `יום ${WEEKDAY[this.weekDay]}, ${gimatria(this.hebDay)} ב${MONTHES[this.hebMonth]} ${gimatria(this.year.hebYear)}`;
+    }
+
+    private getParasha() {
+        const month = MONTHES[this.hebMonth];
+        if(month === YEAR_MONTHS.TESHRI) {
+            if(this.hebDay < 23) return this.getParashaBeforeBershit();
+            else if(this.year.rosh.getDay() === 6 && this.hebDay < 29) {
+                return 'בראשית'
+            }
+        }
+        if (month === YEAR_MONTHS.NISAN) {
+            switch (this.year.getPesahDay()) {
+                case 'א': {
+                    if (this.hebDay >= 15 && this.hebDay <= 21) return 'פסח';
+                    break;
+                }
+                case 'ג': {
+                    if (this.hebDay >= 13 && this.hebDay <= 19) return 'פסח';
+                    break;
+                }
+                case 'ה': {
+                    if (this.hebDay >= 11 && this.hebDay <= 17) return 'פסח';
+                    break;
+                }
+                case 'ז': {
+                    if (this.hebDay >= 9 && this.hebDay <= 15) return 'פסח';
+                    break;
+                }
+                default: {
+                    if (this.day.getDay() == 0) { this.year.weeksWithParash++; }
+                    return parshatShavoa(this.year.type.toString(), this.year.weeksWithParash);
+                }
+            }
+        }
+        if(this.year.getRoshHashanaDay() === 'ז' && (month == MONTHES[13] && this.hebDay >= 24)) return 'ראש השנה';
+        if (this.day.getDay() == 0) { this.year.weeksWithParash++; }
+        return parshatShavoa(this.year.type.toString(), this.year.weeksWithParash)
+
+    }
+
+    /**
+     * get parasha from rosh hashana to simhat tora
+     */
+    private getParashaBeforeBershit(): string {
+        switch (this.year.getRoshHashanaDay()) {
+            case 'ב': {
+                if (this.hebDay >= 1 && this.hebDay <= 6) return 'וילך';
+                else if (this.hebDay >= 7 && this.hebDay <= 13) return 'האזינו';
+                else if (this.hebDay >= 14 && this.hebDay <= 20) return 'סוכות';
+                else return 'בראשית';
+            }
+            case 'ג': {
+                if (this.hebDay >= 1 && this.hebDay <= 5) return 'וילך';
+                else if (this.hebDay >= 6 && this.hebDay <= 12) return 'האזינו';
+                else if (this.hebDay >= 13 && this.hebDay <= 19) return 'סוכות';
+                else return 'בראשית';
+            }
+            case 'ה': {
+                if (this.hebDay >= 1 && this.hebDay <= 3) return 'האזינו';
+                else if (this.hebDay >= 4 && this.hebDay <= 10) return 'יום הכיפורים';
+                else if (this.hebDay >= 11 && this.hebDay <= 17) return 'סוכות';
+                else return 'בראשית';
+            }
+
+            case 'ז': {
+                if (this.hebDay == 1) return 'ראש השנה';
+                else if (this.hebDay >= 2 && this.hebDay <= 8) return 'האזינו';
+                else if (this.hebDay >= 9 && this.hebDay <= 22) return 'סוכות';
+                else return 'בראשית';
+            }
+            default: return '';
         }
     }
 
-    toString(): string {
-        return `יום ${WEEKDAY[this.weekDay]}, ${Common.gim(this.hebDay)} ב${MONTHES[this.hebMonth]} ${Common.gim(this.year)}`;
-    }
 }
